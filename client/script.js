1e9e5c70aacfae32173ef11099ad5be772d7bb16
Masum Gökyüz <masumgokyuz@gmail.com>
2023-08-17 00:08:46 +0300

    prettier fix

--- client/script.js
+++ client/script.js
@@ -1,32 +1,32 @@
 const socket = io("http://localhost:7500");
 
 socket.on("connect", () => {
-	console.log("Connected to the server");
+  console.log("Connected to the server");
 
-	const fetchProductsBtn = document.getElementById("fetchProductsBtn");
-	fetchProductsBtn.addEventListener("click", () => {
-		socket.emit("fetchProducts");
-	});
+  const fetchProductsBtn = document.getElementById("fetchProductsBtn");
+  fetchProductsBtn.addEventListener("click", () => {
+    socket.emit("fetchProducts");
+  });
 
-	const filterXBtn = document.getElementById("filterX");
-	filterXBtn.addEventListener("click", () => {
-		socket.emit("filterProducts", "X");
-	});
+  const filterXBtn = document.getElementById("filterX");
+  filterXBtn.addEventListener("click", () => {
+    socket.emit("filterProducts", "X");
+  });
 
-	const filterMBtn = document.getElementById("filterM");
-	filterMBtn.addEventListener("click", () => {
-		socket.emit("filterProducts", "M");
-	});
+  const filterMBtn = document.getElementById("filterM");
+  filterMBtn.addEventListener("click", () => {
+    socket.emit("filterProducts", "M");
+  });
 });
 
 socket.on("allProducts", (products) => {
-	const productList = document.getElementById("productList");
-	productList.innerHTML = `<h1>All Products (${products.length})</h1>`;
-	products.map((product) => {
-		const listItem = document.createElement("li");
-		listItem.classList.add("product-item");
+  const productList = document.getElementById("productList");
+  productList.innerHTML = `<h1>All Products (${products.length})</h1>`;
+  products.map((product) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("product-item");
 
-		listItem.innerHTML = `
+    listItem.innerHTML = `
             <div class="product-image">
                 <img src="${product.img}" alt="Product Image">
             </div>
@@ -41,18 +41,18 @@ socket.on("allProducts", (products) => {
             </div>
         `;
 
-		productList.appendChild(listItem);
-	});
+    productList.appendChild(listItem);
+  });
 });
 
 socket.on("filteredProducts", (products) => {
-	const productList = document.getElementById("productList");
-	productList.innerHTML = `<h1>Filtered Products (${products.length})</h1>`;
-	products.map((product) => {
-		const listItem = document.createElement("li");
-		listItem.classList.add("product-item");
+  const productList = document.getElementById("productList");
+  productList.innerHTML = `<h1>Filtered Products (${products.length})</h1>`;
+  products.map((product) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("product-item");
 
-		listItem.innerHTML = `
+    listItem.innerHTML = `
             <div class="product-image">
                 <img src="${product.img}" alt="Product Image">
             </div>
@@ -67,6 +67,6 @@ socket.on("filteredProducts", (products) => {
             </div>
         `;
 
-		productList.appendChild(listItem);
-	});
+    productList.appendChild(listItem);
+  });
 });
